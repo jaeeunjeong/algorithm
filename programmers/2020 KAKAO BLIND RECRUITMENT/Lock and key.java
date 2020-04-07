f978095ed15b8cfa96f84a8db6db2ed8a87c871e
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2020-04-07 23:13:42 +0900

    Update and rename Lock and key to Lock and key.java

---
+++ programmers/2020 KAKAO BLIND RECRUITMENT/Lock and key.java
@@ -0,0 +1,49 @@
+class Solution {
+    public static boolean solution(int[][] key, int[][] lock) {
+        boolean answer = false;
+        int[][] extendLock = extendLock(lock);
+        while(!answer){
+	        for (int i = 0; i < key.length; i++) {
+				for (int j = 0; j < key.length; j++) {
+					//검증로직 수정해야함
+					if((extendLock[i][j] + key[i][j]) != 1) {
+                        continue;
+				    }else {
+                        answer = true;
+                        break;
+                    }
+                }
+			}
+	        key = rotation(key);
+        }
+        return answer;
+    }
+    public static int[][] rotation(int[][] key) { //angle = 90 * count
+		int[][] result = new int[key.length][key.length]; 
+    	
+		for (int i = 0; i < result.length; i++) {//행
+			for (int j = 0; j < result.length; j++) {//열
+				result[j][key.length-1- i] = key[i][j];
+			}
+		}
+    	
+    	return result;
+	}
+    public static int[][] extendLock(int[][] lock) { //extend
+    	int length = lock.length + (lock.length - 1) * 2;
+    	int[][] result = new int[length][length]; 
+    	
+    	for (int i = 0; i < lock.length; i++) {
+    		for (int j = 0; j < lock.length; j++) {
+    			result[i+lock.length - 1][lock.length -1 +j] = lock[i][j];
+    		}
+    	}
+    	
+    	return result;
+    }
+public static void main(String[] args) {
+	int[][] key= {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}};
+	int[][] lock = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+	solution(key,lock);
+}
+}
