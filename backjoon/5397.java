1014d4fd3793a1854f90db3be0d77259fa7b0ccc
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2020-06-01 23:45:03 +0900

    Update 5397.java
    Finished!

--- backjoon/5397.java
+++ backjoon/5397.java
@@ -1,42 +1,52 @@
 //https://www.acmicpc.net/problem/5397
+//LinkedList
 import java.util.*;
 import java.io.*;
 public class Main {
-public static void main(String[] args) throws IOException {
+	 public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-		String word = st.nextToken();//input
-		Stack<Character> stack = new Stack();
-		Stack<Character> stackTemp = new Stack();
-		char[] s = word.toCharArray();
-
+		int turn =  Integer.parseInt(st.nextToken());
 		
-		for (int i = 0; i < s.length; i++) {
-			
-			switch (s[i]) {
-			case '<':
-				stack.pop();
-				stackTemp.add(s[i]);
-				break;
-			case '>':
-				
-				stack.pop();
-				break;
-			case '-':
-				
-				stack.pop();
-				break;
-			default:
-				stack.add(s[i]);
-				break;
+		for (int j = 0; j < turn; j++) {
+			//String word = st.nextToken();
+			char[] s = st.nextToken().toCharArray();
+			Stack<Character> stack = new Stack<Character>();
+			Stack<Character> stackTemp = new Stack<Character>();
+
+			for (int i = 0; i < s.length; i++) {
+				switch (s[i]) {
+				case '<':
+					if(!stack.isEmpty()) {
+					stackTemp.push(stack.pop());
+					}
+	  				break;
+				case '>':
+					if(!stackTemp.isEmpty()) {					
+						stack.push(stackTemp.pop());
+					}
+					break;
+				case '-':
+					if(!stack.isEmpty()) {
+						stack.pop();						
+					}
+					break;
+				default:
+					stack.push(s[i]);
+					break;
+				}
 			}
+			while(!stackTemp.isEmpty()) {
+				stack.push(stackTemp.pop());
+			}
+			StringBuilder sb = new StringBuilder();
+                        for(int i=0; i<stack.size(); i++) {
+               	 		sb.append(stack.elementAt(i));
+            		}
+           		System.out.println(sb.toString());
 		}
-		
-
-
-
-		
+				
 		bufferedWriter.flush();
 		bufferedReader.close();
 		bufferedWriter.close();
