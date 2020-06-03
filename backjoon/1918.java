e8154b834b341f35977861f57587233b54bc0136
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2020-06-03 23:11:00 +0900

    Update 1918.java
    Finished!

--- backjoon/1918.java
+++ backjoon/1918.java
@@ -3,40 +3,45 @@
 import java.util.*;
 import java.io.*;
 class Solution{
-	 public static void main(String[] args) throws Exception {
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-
-			//String word = st.nextToken();
-			char[] s = bufferedReader.readLine().toCharArray();
-			Stack<Character> stack = new Stack<Character>();
-			Stack<Character> stackTemp = new Stack<Character>();
-
-			for (int i = 0; i < s.length; i++) {
-				if(s[i]== '+' ||s[i] ==  '-') {
-				
-				}else if(s[i] == '*' || s[i] == '/'){
-					
-				}else if(s[i]== '(') {
-					
-				}else if(s[i]== ')'){
-					
-				}else {
-					stack.push(s[i]);
+	public static void main(String[] args) throws Exception {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		char[] s = bufferedReader.readLine().toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < s.length; i++) {
+			if(s[i] == '*' || s[i] == '/'){
+				while (!stack.isEmpty() &&(stack.peek() == '*' || stack.peek() == '/')) {
+					sb.append(stack.pop());
 				}
+				stack.push(s[i]);  
+			}else if(s[i]== '+' ||s[i] ==  '-') {
+				while (!stack.isEmpty() && 
+				       (stack.peek() == '*' || stack.peek() == '/' 
+					|| stack.peek() == '+'  || stack.peek() == '-')) {
+					sb.append(stack.pop());
+				}
+				stack.push(s[i]);  
+			}else if(s[i]== '(') {//여는 괄호
+				stack.add(s[i]);
+			}else if(s[i]== ')'){//닫는 괄호
+             	  	 	while(!stack.isEmpty() && stack.peek() != '('){
+                    			sb.append(stack.pop());
+               		}
+                    		stack.pop();
+			}else {//피연산자 -> 바로 출력
+				sb.append(s[i]);
 			}
-            StringBuilder sb = new StringBuilder();
-            
-            for(int i=0; i<stack.size(); i++) {
-                sb.append(stack.elementAt(i));
-            }
-            System.out.println(sb.toString());
-			
-			
-			
-			bufferedWriter.flush();
-			bufferedReader.close();
-			bufferedWriter.close();
-	        
-	    }
+		}
+       		while(!stack.isEmpty()){
+            		sb.append(stack.pop());
+        	}
+		
+       		System.out.println(sb.toString());
+		bufferedWriter.flush();
+		bufferedReader.close();
+		bufferedWriter.close();
+        
+    }
 }
