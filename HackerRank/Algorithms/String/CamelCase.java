29c7d8f8a34b4745ca5325736f86f4b0f707f66d
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2020-07-09 16:40:02 +0900

    Create CamelCase.java

---
+++ HackerRank/Algorithms/String/CamelCase.java
@@ -0,0 +1,36 @@
+//https://www.hackerrank.com/challenges/camelcase/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the camelcase function below.
+    static int camelcase(String s) {              
+        s.matches("([A-Z])");//정규식 
+        String []resultArr = s.split("([A-Z])");
+        
+        return resultArr.length;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
