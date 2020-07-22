c662e304210d2b3980daba4fcbf7bbc19463710a
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2020-07-22 23:50:16 +0900

    Create Equalize the Array.java
    https://www.hackerrank.com/challenges/equality-in-a-array

---
+++ HackerRank/Implementation/Equalize the Array.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        int result = 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i= 0; i< arr.length; i++){
+            int cnt = 1;
+            if(map.containsKey(arr[i])) {
+                cnt = map.get(arr[i]);
+                map.replace(arr[i], ++cnt);
+                if(cnt > result) {
+                    result = cnt;
+                                    
+                }
+            }else {
+                map.put(arr[i], cnt);
+            }
+        }
+
+        result = arr.length - result;
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
