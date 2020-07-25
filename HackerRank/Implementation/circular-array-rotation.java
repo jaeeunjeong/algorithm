2ea2a96c43f42d499c949ef2d35ed7688e3e7d8c
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2020-07-25 23:42:29 +0900

    Create circular-array-rotation.java
    https://www.hackerrank.com/challenges/circular-array-rotation/problem
    java8

---
+++ HackerRank/Implementation/circular-array-rotation.java
@@ -0,0 +1,78 @@
+//https://www.hackerrank.com/challenges/circular-array-rotation/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the circularArrayRotation function below.
+    static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+        int []result = new int[queries.length];
+        int []temp = new int[a.length] ;
+        //a : query k : query: new array.
+
+        for (int i = 0; i < a.length; i++) { 
+            temp[(i+k) % a.length] = a[i];
+        }
+        
+        for (int i = 0; i < queries.length; i++) {
+            int n = queries[i];
+            result[i] = temp[n];
+        }
+         
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nkq = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nkq[0]);
+
+        int k = Integer.parseInt(nkq[1]);
+
+        int q = Integer.parseInt(nkq[2]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++) {
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
