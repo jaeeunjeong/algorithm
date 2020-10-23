d0e2c6496de8beffa9f3f09c9437b4210227dfef
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2020-10-23 18:17:17 +0900

    Update 소수 찾기.java
    참고하면 좋은 permutation

--- programmers/코딩테스트 연습/완전탐색/소수 찾기.java
+++ programmers/코딩테스트 연습/완전탐색/소수 찾기.java
@@ -63,3 +63,16 @@ class Solution {
     	return result;
     }
 }
+/*
+        public void permutation(String prefix, String str, HashSet<Integer> set) {
+        int n = str.length();
+        //if (n == 0) System.out.println(prefix);
+        if(!prefix.equals("")) set.add(Integer.valueOf(prefix));
+        for (int i = 0; i < n; i++)
+          permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n), set);
+	}
+
+
+
+
+*/
