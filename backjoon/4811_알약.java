e9ae0469eb4ce1b332279789e56b3cead9b39269
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2022-01-04 23:39:51 +0900

    Update 4811_알약.java
    오타 수정

--- backjoon/4811_알약.java
+++ backjoon/4811_알약.java
@@ -23,7 +23,7 @@ public class Main {
 		for (int h = 0; h <= 30; h++) {
 			for (int w = 0; w <= 30; w++) {
 				if(h > w) continue; // 1.
-				if(h == 0) dp[h][w] = 1;
+				if(h == 0) dp[w][h] = 1;
 				else dp[w][h] = dp[w-1][h] + dp[w][h-1];//2.
 			}
 		}
