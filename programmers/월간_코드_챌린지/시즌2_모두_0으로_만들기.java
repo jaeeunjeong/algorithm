041b558d75336314512923b86ee9b01df9535333
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2022-01-08 22:34:03 +0900

    Create 시즌2_모두_0으로_만들기.java

---
+++ programmers/월간_코드_챌린지/시즌2_모두_0으로_만들기.java
@@ -0,0 +1,50 @@
+//https://programmers.co.kr/learn/courses/30/lessons/76503
+import java.util.*; 
+/**
+* 트리 + DFS
+* 1. 트리 구조를 만들어준다.
+* 2. DFS를 이용하여 루트에서 제일 밑의 노드까지 탐색하면서 그 곳에서 0을 만들어주면서 올라온다.
+*
+* 주의할 점은 연산하는 과정에서 가중치 배열이 int 범위를 넘어갈 수 있어서 long 타입으로 변경해줘야한다는 것.
+* 
+*/
+class Solution {
+    static ArrayList<Integer>[] list;
+    static long answer;
+    public long solution(int[] a, int[][] edges) {
+        answer = -2;
+        int length = a.length;
+        list = new ArrayList[length];
+        long isZero = 0;
+        long[] temp = new long[length];
+        for(int i = 0; i < length; i++){
+            list[i] = new ArrayList<Integer>();
+            int cur = a[i];
+            temp[i] = cur;
+            isZero += cur;
+        }
+        
+        for(int[] e : edges){
+            list[e[0]].add(e[1]);
+            list[e[1]].add(e[0]);
+        }
+        if(isZero == 0L ) answer = dfs(temp, 0, -1);
+        else answer = -1;
+        return answer;
+    }
+    
+    public static long dfs(long[] a, int cur, int parent){
+        long result = 0;
+        
+        for(int i = 0; i < list[cur].size(); i++){
+            int next = list[cur].get(i);
+            if(next != parent){
+                result += dfs(a, next, cur);
+            }
+        }
+        
+        if(parent != -1) a[parent] += a[cur];
+        
+        return result += Math.abs(a[cur]);
+    }
+}
