71db5864180fca69264a1328a703cb51414633a2
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2022-01-21 21:41:51 +0900

    Update 11053_가장_긴_증가하는_부분_수열.java

--- backjoon/11053_가장_긴_증가하는_부분_수열.java
+++ backjoon/11053_가장_긴_증가하는_부분_수열.java
@@ -7,6 +7,11 @@ import java.util.*;
  * 현재 값을 기준으로 작은 값 중에서 길이가 제일 긴 값을 저장해서
  * 그 값에  +1한 값을 현재 값의 길이를 저장하는 배열에 넣어준다.
  * 메모이제이션 문제
+ 
+ * - 2022.01.21 추가.
+ * 범위를 점점 늘려가면서 현재 값보다 작은 값이 있는 지 확인한다.
+ * 중간에 작아지는 값이 있다면 그 값은 여태의 값 중에서 제일 큰 값을 가져가고 무시된다.
+ * 값이 커지면 +1 해주기!!!!
 **/
 class Main {
 	public static void main(String[] args) throws Exception {
@@ -31,7 +36,7 @@ class Main {
 			for (int j = 0; j < i; j++) {
 				if(arr[j] < arr[i]) max = Math.max(count[j], max);
 			}
-			count[i] = max+1;
+			count[i] = max+1;//제일 뒤에 값이 클 것이라고 가정하고 풀이하기 떄문.
 			answer = Math.max(count[i], answer);
 		}
 		System.out.println(answer);
