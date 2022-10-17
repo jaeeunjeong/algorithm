bbe57e95af1d221ff6cb06aaf72589bcbaec2fff
jaeeunjeong <49487155+jaeeunjeong@users.noreply.github.com>
2022-10-17 23:06:20 +0900

    Update 0_21611_마법사_상어와_블리자드.java

--- backjoon/0_21611_마법사_상어와_블리자드.java
+++ backjoon/0_21611_마법사_상어와_블리자드.java
@@ -1,20 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
+	// static int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 	static int[][] map;
-	static int[] arr, answer;
-	static int N;
+	static int[] arrA, arrB, ans;
+	static int N, M;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		map = new int[N][N];
-		answer = new int[4];
-		arr = new int[N * N];
+		arrA = new int[N * N];
+		arrB = new int[N * N];
+		ans = new int[4];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
@@ -23,111 +25,104 @@ class Main {
 		}
 		while (M-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int d = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken()) - 1;
 			int s = Integer.parseInt(st.nextToken());
-			step1(d, s); // 해당 방향으로 파괴하기
+			blizad(d, s);
 			// print();
-			// 빈칸으로 당겨서 큐로 만들어서 4개 이상은 없애기.
-			makeQueue();
-			while (step2());
-			// print();
-			// 구슬 새로 만들기
-			int[] result = step3();
-			// print();
-			// 다시 2차원으로 만들기.
-			makeMap(result);
+			// printArr();
+			compress();
+			// printArr();
+			while (bomb())
+				;
+			change();
+//			print();
 		}
-		int answer = cal();
+		int answer = ans[1] * 1 + ans[2] * 2 + ans[3] * 3;
 		System.out.println(answer);
 	}
 
-	private static int cal() {
-		return answer[1] * 1 + answer[2] * 2 + answer[3] * 3;
+	private static void print() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
-	private static void makeMap(int[] arr) {
+	private static void printArr() {
+		for (int i = 0; i < N * N; i++) {
+			System.out.print(arrA[i] + " ");
+		}
+		System.out.println();
+	}
 
+	// 구슬 숫자와 갯수 파악해서 새로운 배열만드는 메서드
+	private static void change() {
+		arrB = new int[N * N];
+		int idx = 0;
+		for (int i = 0; i < N * N;) {
+			int j = i + 1;
+			int cnt = 1;
+			if(arrA[i] == 0) break;
+			while (j < N * N && arrA[i] == arrA[j++])
+				cnt++;
+			arrB[idx++] = cnt;
+			arrB[idx++] = arrA[i];
+			if (idx == N * N -1)
+				break;
+			i = j -1;
+		}
+		for (int i = 0; i < N * N; i++) {
+			arrA[i] = arrB[i];
+		}
+		// 다시 달팽이에 넣어주기.
+		int[][] dirs = new int[][] { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+		int d = 0;
+		idx = 0;
+		int length = 1;
 		int nextR = N / 2;
 		int nextC = N / 2;
-
-		int length = 1;
-		int d = 0;
-		int[][] dirs = new int[][] { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
-		int idx = 0;
-		while (length < N) {
-
-			for (int i = 0; i < length; i++) {
-				nextR += dirs[d][0];
-				nextC += dirs[d][1];
-				map[nextR][nextC] = arr[idx++];
+		while (idx < N * (N - 1)) {
+			for (int times = 0; times < 2; times++) {
+				for (int i = 0; i < length; i++) {
+					nextR += dirs[d][0];
+					nextC += dirs[d][1];
+					map[nextR][nextC] = arrA[idx++];
+				}
+				d = (d + 1) % 4;
+				for (int i = 0; i < length; i++) {
+					nextR += dirs[d][0];
+					nextC += dirs[d][1];
+					map[nextR][nextC] = arrA[idx++];
+				}
+				d = (d + 1) % 4;
+				length++;
 			}
-			d = (d + 1) % 4;
-			for (int i = 0; i < length; i++) {
-				nextR += dirs[d][0];
-				nextC += dirs[d][1];
-				map[nextR][nextC] = arr[idx++];
-			}
-			d = (d + 1) % 4;
-			length++;
 		}
-
-		// 마지막은 따로 넣어주기.
 		for (int i = 0; i < length - 1; i++) {
 			nextR += dirs[d][0];
 			nextC += dirs[d][1];
-			map[nextR][nextC] = arr[idx++];
+			map[nextR][nextC] = arrA[idx++];
 		}
 	}
 
-	private static int[] step3() {
-		int[] result = new int[N * N * N];
-		int idx = 0;
-		for (int i = 0; i < N * N; i++) {
-			int j = 0;
-			int cnt = 0;
-			if (arr[i] == 0)
-				break;
-			for (j = i; j < N * N; j++) {
-				if (arr[i] == arr[j])
-					cnt++;
-				else
-					break;
-			}
-
-			result[idx++] = cnt;
-			result[idx++] = arr[i];
-			i = j - 1;
-
-		}
-		return result;
-	}
-	private static void compress() {
-		int last = 0;
-		for (int i = 0; i < N * N; i++) {
-			if (arr[i] == 0)
-				continue;
-			arr[last++] = arr[i];
-		}
-		for (int i = last; i < N * N; i++) {
-			arr[i] = 0;
-		}
-	}
 	// 4개이상 연속하는 것 폭발
-	private static boolean step2() {
+	private static boolean bomb() {
 		boolean flag = false;
 		for (int i = 0; i < N * N;) {
 			int j = i + 1;
-			if (arr[i] == 0)
+			if (arrA[i] == 0)
 				break;
-			while (arr[i] == arr[j]) {
+			while (arrA[i] == arrA[j]) {
 				j++;
 			}
 
 			if (j - i >= 4) {
 				flag = true;
 				for (int k = i; k < j; k++) {
-					answer[arr[k]]++;
-					arr[k] = 0;
+					ans[arrA[k]]++;
+					arrA[k] = 0;
 				}
 			}
 			i = j;
@@ -138,68 +133,54 @@ class Main {
 		return flag;
 	}
 
-	private static int getStartIdx() { 
+	// 0을 당기기
+	private static void compress() {
+		int last = 0;
 		for (int i = 0; i < N * N; i++) {
-			if (arr[i] == 0)
+			if (arrA[i] == 0)
 				continue;
-			int cnt = 0;
-			for (int j = i + 1; j < N * N; j++) {
-				if (arr[i] == arr[j])
-					cnt++;
-				else
-					break;
-				if (cnt >= 3) {
-					return i;
-				}
-			}
+			arrA[last++] = arrA[i];
 		}
-		return -1;
-	}
-
-	// 달팽이 돌기.
-	private static void makeQueue() {
-
-		int nextR = N / 2;
-		int nextC = N / 2;
-
-		int length = 1;
-		int d = 0;
-		int idx = 0;
-		int[][] dirs = new int[][] { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
-		while (length < N) {
-			for (int i = 0; i < length; i++) {
-				nextR += dirs[d][0];
-				nextC += dirs[d][1];
-				if (map[nextR][nextC] == 0)
-					continue;
-				arr[idx++] = map[nextR][nextC];
-			}
-			d = (d + 1) % 4;
-			for (int i = 0; i < length; i++) {
-				nextR += dirs[d][0];
-				nextC += dirs[d][1];
-				if (map[nextR][nextC] == 0)
-					continue;
-				arr[idx++] = map[nextR][nextC];
-			}
-			d = (d + 1) % 4;
-			length++;
-		}
-
-		// 마지막은 따로 넣어주기.
-		for (int i = 0; i < length - 1; i++) {
-			nextR += dirs[d][0];
-			nextC += dirs[d][1];
-			arr[idx++] = map[nextR][nextC];
+		for (int i = last; i < N * N; i++) {
+			arrA[i] = 0;
 		}
 	}
 
-	private static void step1(int d, int s) {
-		int[][] dirs = new int[][] { {}, { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	private static void blizad(int d, int s) {
+		int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 		for (int i = 1; i <= s; i++) {
 			int nextR = N / 2 + dirs[d][0] * i;
 			int nextC = N / 2 + dirs[d][1] * i;
 			map[nextR][nextC] = 0;
+		}
+		// 1차원으로 가져오기.
+		int nextR = N / 2;
+		int nextC = N / 2;
+		int length = 1;
+		int idx = 0;
+		dirs = new int[][] { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+		d = 0;
+		while (idx < N * (N - 1)) {
+			for (int times = 0; times < 2; times++) {
+				for (int i = 0; i < length; i++) {
+					nextR += dirs[d][0];
+					nextC += dirs[d][1];
+					arrA[idx++] = map[nextR][nextC];
+				}
+				d = (d + 1) % 4;
+				for (int i = 0; i < length; i++) {
+					nextR += dirs[d][0];
+					nextC += dirs[d][1];
+					arrA[idx++] = map[nextR][nextC];
+				}
+				d = (d + 1) % 4;
+				length++;
+			}
+		}
+		for (int i = 0; i < length - 1; i++) {
+			nextR += dirs[d][0];
+			nextC += dirs[d][1];
+			arrA[idx++] = map[nextR][nextC];
 		}
 	}
 }
